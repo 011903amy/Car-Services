b4ca9f47b00420a0a53e5bfa52fd5f7a3605e9d7
saciinol <darrenicolas18@gmail.com>
2024-10-04 15:56:13 +0800

    updated

---
+++ js/slider.js
@@ -0,0 +1,12 @@
+// var slider = tns({
+//   container: "",
+//   items: 3,
+//   speed: 200,
+//   gutter: 20,
+//   slideBy: 1,
+//   autoplay: false,
+//   navPostion: "bottom",
+//   controls: "#controls",
+//   prevButton: ".prev",
+//   nextButton: ".next",
+// });
