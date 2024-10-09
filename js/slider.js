4cd56219d1a290b8c8a3d27951a5c899bb8be494
KmEnriquez <kentmathewenriquez@gmail.com>
2024-10-09 15:30:49 +0800

    merge updated

--- js/slider.js
+++ js/slider.js
@@ -1,26 +1,5 @@
 
 
-// var slider = tns({
-//   container: ".services__features--details",
-//   items: 3,
-//   speed: 300,
-//   gutter: 30,
-//   slideBy: 1,
-//   autoplay: false,
-//   navPosition: "bottom",
-//   contianerControls: "#controls",
-//   prevButton: ".prev",
-//   nextButton: ".next",
-//   responsive: {
-//     1250: {
-//       items: 1,
-//     },
-//     200: {
-//       items: 1,
-//     },
-//   },
-// });
-
 var slider = tns({
   container: ".services__card",
   items: 3,
@@ -32,35 +11,12 @@ var slider = tns({
   controls: "#controls",
   prevButton: ".prev",
   nextButton: ".next",
-  responsive:{
-    1250:{
-      items: 3,
-
-    },
-    200:{
-      items:2,
-    }
-
-  }
-});
-var slider = tns({
-  container: ".headerBanner__slider",
-  items: 1,
-  axis: "vertical",
-  speed: 200,
-  slideBy: "page",
-  autoHeight: true,
-  autoplay: true,
-  navPosition: "bottom",
-  controls: false,
-
   responsive: {
     1250: {
-      items: 1,
+      items: 3,
     },
     200: {
-      items: 1,
+      items: 2,
     },
   },
 });
-
